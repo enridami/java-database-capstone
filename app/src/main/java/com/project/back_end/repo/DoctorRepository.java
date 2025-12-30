5e8709577ae49a0161bd415ed4810072d846ba49
Enrique Suica <enri124589@gmail.com>
2025-12-30 09:29:56 -0300

    Reorder imports in DoctorRepository.java
    Adjusted the import statements for better organization and adherence to standard Java import ordering.

--- app/src/main/java/com/project/back_end/repo/DoctorRepository.java
+++ app/src/main/java/com/project/back_end/repo/DoctorRepository.java
@@ -1,10 +1,11 @@
 package com.project.back_end.repo;
 
-import com.project.back_end.models.Doctor;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.project.back_end.models.Doctor;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
